9a66e033720b46e39421fe261aeb32d4116fd278
bst2017 <worldprc@qq.com>
2017-11-27 15:28:01 +0800

    Add notes for it's compatibility with Opencv 2.4

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -25,83 +25,65 @@ using namespace DBoW2;
 using namespace DUtils;
 using namespace std;
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
 void loadFeatures(vector<vector<cv::Mat > > &features);
 void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out);
 void testVocCreation(const vector<vector<cv::Mat > > &features);
 void testDatabase(const vector<vector<cv::Mat > > &features);
+void wait();
 
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-// number of training images
-const int NIMAGES = 4;
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-void wait()
-{
-  cout << endl << "Press enter to continue" << endl;
-  getchar();
-}
-
-// ----------------------------------------------------------------------------
+const int NIMAGES = 4; // number of training images
 
 int main()
 {
   vector<vector<cv::Mat > > features;
-  loadFeatures(features);
+  loadFeatures(features); // 计算并保存输入训练图像的特征
 
-  testVocCreation(features);
+  testVocCreation(features); // 测试训练出来的字典，并保存该字典
 
   wait();
 
-  testDatabase(features);
+  testDatabase(features); // 测试数据集
 
   return 0;
 }
 
-// ----------------------------------------------------------------------------
-
+//-------------------------------成员函数----------------------------------
 void loadFeatures(vector<vector<cv::Mat > > &features)
 {
   features.clear();
   features.reserve(NIMAGES);
 
-  cv::Ptr<cv::ORB> orb = cv::ORB::create();
+  cv::Ptr<cv::ORB> orb = cv::ORB::create(); // opencv3.1及以上版本的写法
+  cv::ORB orb(500); // opencv 2.4.8版本的写法
+                    // 其余参数均为默认值
 
   cout << "Extracting ORB features..." << endl;
   for(int i = 0; i < NIMAGES; ++i)
   {
     stringstream ss;
-    ss << "images/image" << i << ".png";
+    ss << "images/image" << i << ".png"; // ss<<“路径/中文名”<<序号<<格式后缀
 
     cv::Mat image = cv::imread(ss.str(), 0);
     cv::Mat mask;
     vector<cv::KeyPoint> keypoints;
     cv::Mat descriptors;
 
-    orb->detectAndCompute(image, mask, keypoints, descriptors);
+    orb->detectAndCompute(image, mask, keypoints, descriptors); // opencv3.1及以上版本的写法
+    orb.detect(image, keypoints); // opencv 2.4.8版本的写法
+    orb.compute(image, keypoints, descriptors); // opencv 2.4.8版本的写法
 
     features.push_back(vector<cv::Mat >());
     changeStructure(descriptors, features.back());
   }
 }
 
-// ----------------------------------------------------------------------------
-
 void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out)
 {
   out.resize(plain.rows);
 
   for(int i = 0; i < plain.rows; ++i)
-  {
     out[i] = plain.row(i);
-  }
 }
-
-// ----------------------------------------------------------------------------
 
 void testVocCreation(const vector<vector<cv::Mat > > &features)
 {
@@ -111,6 +93,13 @@ void testVocCreation(const vector<vector<cv::Mat > > &features)
   const WeightingType weight = TF_IDF;
   const ScoringType score = L1_NORM;
 
+ /**
+   * Initiates an empty vocabulary
+   * @param k branching factor
+   * @param L depth levels
+   * @param weighting weighting type
+   * @param scoring scoring type
+   */
   OrbVocabulary voc(k, L, weight, score);
 
   cout << "Creating a small " << k << "^" << L << " vocabulary..." << endl;
@@ -120,7 +109,7 @@ void testVocCreation(const vector<vector<cv::Mat > > &features)
   cout << "Vocabulary information: " << endl
   << voc << endl << endl;
 
-  // lets do something with this vocabulary
+  // let‘s do something with this vocabulary
   cout << "Matching images against themselves (0 low, 1 high): " << endl;
   BowVector v1, v2;
   for(int i = 0; i < NIMAGES; i++)
@@ -135,13 +124,16 @@ void testVocCreation(const vector<vector<cv::Mat > > &features)
     }
   }
 
-  // save the vocabulary to disk
   cout << endl << "Saving vocabulary..." << endl;
   voc.save("small_voc.yml.gz");
   cout << "Done" << endl;
 }
 
-// ----------------------------------------------------------------------------
+void wait()
+{
+  cout << endl << "Press enter to continue" << endl;
+  getchar();
+}
 
 void testDatabase(const vector<vector<cv::Mat > > &features)
 {
@@ -150,17 +142,13 @@ void testDatabase(const vector<vector<cv::Mat > > &features)
   // load the vocabulary from disk
   OrbVocabulary voc("small_voc.yml.gz");
   
-  OrbDatabase db(voc, false, 0); // false = do not use direct index
-  // (so ignore the last param)
-  // The direct index is useful if we want to retrieve the features that 
-  // belong to some vocabulary node.
+  OrbDatabase db(voc, false, 0); // false = do not use direct index (so ignore the last param)
+  // The direct index is useful if we want to retrieve the features that belong to some vocabulary node.
   // db creates a copy of the vocabulary, we may get rid of "voc" now
 
   // add images to the database
   for(int i = 0; i < NIMAGES; i++)
-  {
     db.add(features[i]);
-  }
 
   cout << "... done!" << endl;
 
@@ -176,24 +164,18 @@ void testDatabase(const vector<vector<cv::Mat > > &features)
 
     // ret[0] is always the same image in this case, because we added it to the 
     // database. ret[1] is the second best match.
-
     cout << "Searching for Image " << i << ". " << ret << endl;
   }
 
   cout << endl;
 
-  // we can save the database. The created file includes the vocabulary
-  // and the entries added
+  // we can save the database. The created file includes the vocabulary and the entries added
   cout << "Saving database..." << endl;
   db.save("small_db.yml.gz");
   cout << "... done!" << endl;
   
-  // once saved, we can load it again  
-  cout << "Retrieving database once again..." << endl;
-  OrbDatabase db2("small_db.yml.gz");
-  cout << "... done! This is: " << endl << db2 << endl;
+  // // once saved, we can load it again  
+  // cout << "Retrieving database once again..." << endl;
+  // OrbDatabase db2("small_db.yml.gz");
+  // cout << "... done! This is: " << endl << db2 << endl;
 }
-
-// ----------------------------------------------------------------------------
-
-
