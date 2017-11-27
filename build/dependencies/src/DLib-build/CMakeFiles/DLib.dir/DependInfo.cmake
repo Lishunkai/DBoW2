2d5adc33b77b0f8e47e32ce52953494a306e1481
bst2017 <worldprc@qq.com>
2017-11-27 15:26:10 +0800

    build

---
+++ build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/BinaryFile.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/BinaryFile.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/ConfigFile.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/ConfigFile.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/DebugFunctions.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/DebugFunctions.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/FileFunctions.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/FileFunctions.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/LUT.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/LUT.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/LineFile.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/LineFile.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/Profiler.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/Profiler.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/Random.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/Random.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/StringFunctions.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/StringFunctions.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/TimeManager.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/TimeManager.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtils/Timestamp.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtils/Timestamp.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtilsCV/Drawing.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtilsCV/Drawing.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtilsCV/GUI.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtilsCV/GUI.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtilsCV/Geometry.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtilsCV/Geometry.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtilsCV/IO.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtilsCV/IO.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtilsCV/Mat.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtilsCV/Mat.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtilsCV/Transformations.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtilsCV/Transformations.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DUtilsCV/Types.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DUtilsCV/Types.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/BRIEF.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/BRIEF.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/BundleCamera.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/BundleCamera.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/FSolver.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/FSolver.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/HSolver.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/HSolver.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/ImageFunctions.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/ImageFunctions.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/Matches.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/Matches.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/PLYFile.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/PLYFile.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/PMVSCamera.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/PMVSCamera.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/PatchFile.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/PatchFile.cpp.o"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/src/DVision/PixelPointFile.cpp" "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib-build/CMakeFiles/DLib.dir/src/DVision/PixelPointFile.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "OPENCV_TRAITS_ENABLE_DEPRECATED"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/include/DUtils"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/include/DUtilsCV"
+  "/home/bst2017/slam/DBoW2/build/dependencies/src/DLib/include/DVision"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
